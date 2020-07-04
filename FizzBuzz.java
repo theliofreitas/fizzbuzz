eb2c2c5e286553d295eb6e5075ec6ddb557b48ba
Thélio Freitas <marquesthelio@hotmail.com>
2020-07-04 18:10:47 -0300

    Second approach

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,24 +2,18 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            String output = checkNumber(i);
-            System.out.println(output);
+            String output = "";
+            output += checkNumber(i, 3, "Fizz");
+            output += checkNumber(i, 5, "Buzz");
+            System.out.println(output.isEmpty() ? i : output);
         }
     }
 
-    public static String checkNumber(int number) {
-        String result = "";
-
-        if(number % 3 == 0){
-            result += "Fizz";
+    public static String checkNumber(int number, int divisor, String output) {
+        if(number % divisor == 0){
+            return output;
         }
-        if(number % 5 == 0){
-            result += "Buzz";
-        }
-        if(result == ""){
-            result = Integer.toString(number);
-        }
-        return result;
+        return "";
     }
     
 }
